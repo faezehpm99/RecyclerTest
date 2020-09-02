0318b8f632e6cac6f66129fb5a4309021c55d343
Sarv <pirmohamadifaezeh@yahoo.com>
2020-09-02 23:40:46 +0430

    just make first page for getting input

---
+++ app/src/main/java/com/example/recyclertest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.recyclertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        enteranceFragment fragment = new enteranceFragment();
+        fragmentTransaction.add(R.id.FragmentContainer,fragment);
+        fragmentTransaction.commit();
+    }
+}
